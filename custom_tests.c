4af5620af1c7bfc50d13a9e3a5097598d66b6fd4
gaoyulong <gaoyulong@xiaomi.com>
2023-04-28 16:39:34 +0800

    accomplish proj1

--- custom_tests.c
+++ custom_tests.c
@@ -98,36 +98,240 @@ bool test_is_vowel() {
 
 bool test_is_tail() {
   // TODO: Implement this function.
+  if (!assert_true("is_tail_1", is_tail('w'))) {
+    return false;
+  }
+  if (!assert_true("is_tail_2", is_tail('a'))) {
+    return false;
+  }
+  if (!assert_true("is_tail_3", is_tail('s'))) {
+    return false;
+  }
+  if (!assert_true("is_tail_4", is_tail('d'))) {
+    return false;
+  }
+  if (!assert_false("is_tail_5", is_tail('W'))) {
+    return false;
+  }
+  if (!assert_false("is_tail_6", is_tail('<'))) {
+    return false;
+  }
   return true;
 }
 
 bool test_is_head() {
   // TODO: Implement this function.
+  if (!assert_true("is_head_1", is_head('W'))) {
+    return false;
+  }
+  if (!assert_true("is_head_2", is_head('A'))) {
+    return false;
+  }
+  if (!assert_true("is_head_3", is_head('S'))) {
+    return false;
+  }
+  if (!assert_true("is_head_4", is_head('D'))) {
+    return false;
+  }
+  if (!assert_false("is_head_5", is_head('s'))) {
+    return false;
+  }
+  if (!assert_false("is_head_6", is_head('<'))) {
+    return false;
+  }
+  if (!assert_true("is_head_7", is_head('x'))) {
+    return false;
+  }
   return true;
 }
 
 bool test_is_snake() {
   // TODO: Implement this function.
+  if (!assert_true("is_snake_1", is_snake('W'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_2", is_snake('A'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_3", is_snake('S'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_4", is_snake('D'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_5", is_snake('w'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_6", is_snake('a'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_7", is_snake('s'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_8", is_snake('d'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_9", is_snake('^'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_10", is_snake('v'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_11", is_snake('<'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_12", is_snake('>'))) {
+    return false;
+  }
+  if (!assert_true("is_snake_13", is_snake('x'))) {
+    return false;
+  }
+  if (!assert_false("is_snake_14", is_snake('#'))) {
+    return false;
+  }
+  if (!assert_false("is_snake_15", is_snake('*'))) {
+    return false;
+  }
   return true;
 }
 
 bool test_body_to_tail() {
   // TODO: Implement this function.
+  if (!assert_true("body_to_tail_1", body_to_tail('^') == 'w')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_2", body_to_tail('<') == 'a')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_3", body_to_tail('>') == 'd')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_4", body_to_tail('v') == 's')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_5", body_to_tail('w') == '?')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_6", body_to_tail('a') == '?')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_7", body_to_tail('s') == '?')) {
+    return false;
+  }
+  if (!assert_true("body_to_tail_8", body_to_tail('d') == '?')) {
+    return false;
+  }
+  if (!assert_false("body_to_tail_9", body_to_tail('^') == '?')) {
+    return false;
+  }
+  if (!assert_false("body_to_tail_10", body_to_tail('V') == 's')) {
+    return false;
+  }
   return true;
 }
 
 bool test_head_to_body() {
   // TODO: Implement this function.
+  if (!assert_true("head_to_body_1", head_to_body('W') == '^')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_2", head_to_body('A') == '<')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_3", head_to_body('S') == 'v')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_4", head_to_body('D') == '>')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_5", head_to_body('w') == '?')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_6", head_to_body('a') == '?')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_7", head_to_body('s') == '?')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_8", head_to_body('d') == '?')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_9", head_to_body('^') == '?')) {
+    return false;
+  }
+  if (!assert_true("head_to_body_10", head_to_body('V') == '?')) {
+    return false;
+  }
   return true;
 }
 
 bool test_get_next_x() {
   // TODO: Implement this function.
+  if (!assert_true("get_next_row_1", get_next_row(10, 'W') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_2", get_next_row(10, 'A') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_3", get_next_row(10, 'S') == 11)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_4", get_next_row(10, 'D') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_5", get_next_row(10, 'w') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_6", get_next_row(10, 'a') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_7", get_next_row(10, 's') == 11)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_8", get_next_row(10, 'd') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_9", get_next_row(10, '^') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_row_10", get_next_row(10, 'V') == 10)) {
+    return false;
+  }
   return true;
 }
 
 bool test_get_next_y() {
   // TODO: Implement this function.
+  if (!assert_true("get_next_col_1", get_next_col(10, 'W') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_2", get_next_col(10, 'A') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_3", get_next_col(10, 'S') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_4", get_next_col(10, 'D') == 11)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_5", get_next_col(10, 'w') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_6", get_next_col(10, 'a') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_7", get_next_col(10, 's') == 10)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_8", get_next_col(10, 'd') == 11)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_9", get_next_col(10, '<') == 9)) {
+    return false;
+  }
+  if (!assert_true("get_next_col_10", get_next_col(10, '>') == 11)) {
+    return false;
+  }
   return true;
 }
 
--- state.c
+++ state.c
@@ -21,6 +21,7 @@ static char next_square(game_state_t* state, unsigned int snum);
 static void update_tail(game_state_t* state, unsigned int snum);
 static void update_head(game_state_t* state, unsigned int snum);
 
+#define MAX_LINE_LEN 131072
 /* Task 1 */
 game_state_t* create_default_state() {
   // TODO: Implement this function.
@@ -122,6 +123,13 @@ static void set_board_at(game_state_t* state, unsigned int row, unsigned int col
   state->board[row][col] = ch;
 }
 
+static inline bool in_chars(char c, char* chars) {
+  while (*chars) {
+    if (c == *chars) return true;
+    chars++;
+  }
+  return false;
+}
 /*
   Returns true if c is part of the snake's tail.
   The snake consists of these characters: "wasd"
@@ -129,7 +137,7 @@ static void set_board_at(game_state_t* state, unsigned int row, unsigned int col
 */
 static bool is_tail(char c) {
   // TODO: Implement this function.
-  return true;
+  return in_chars(c, "wasd");
 }
 
 /*
@@ -139,7 +147,7 @@ static bool is_tail(char c) {
 */
 static bool is_head(char c) {
   // TODO: Implement this function.
-  return true;
+  return in_chars(c, "WASDx");
 }
 
 /*
@@ -148,7 +156,7 @@ static bool is_head(char c) {
 */
 static bool is_snake(char c) {
   // TODO: Implement this function.
-  return true;
+  return in_chars(c, "wasd^<v>WASDx");
 }
 
 /*
@@ -158,6 +166,18 @@ static bool is_snake(char c) {
 */
 static char body_to_tail(char c) {
   // TODO: Implement this function.
+  if (c == '>') {
+    return 'd';
+  } else if (c == '<') {
+    return 'a';
+  } else if (c == '^') {
+    return 'w';
+  } else if (c == 'v') {
+    return 's';
+  } else {
+    fprintf(stderr, "body_to_tail: unknown body char: %c\n", c);
+    return '?';
+  }
   return '?';
 }
 
@@ -168,6 +188,18 @@ static char body_to_tail(char c) {
 */
 static char head_to_body(char c) {
   // TODO: Implement this function.
+  if (c == 'W') {
+    return '^';
+  } else if (c == 'A') {
+    return '<';
+  } else if (c == 'S') {
+    return 'v';
+  } else if (c == 'D') {
+    return '>';
+  } else {
+    fprintf(stderr, "head_to_body: unknown head char: %c\n", c);
+    return '?';
+  }
   return '?';
 }
 
@@ -178,6 +210,11 @@ static char head_to_body(char c) {
 */
 static unsigned int get_next_row(unsigned int cur_row, char c) {
   // TODO: Implement this function.
+  if (in_chars(c, "vsS")) {
+    return cur_row + 1;
+  } else if (in_chars(c, "^wW")) {
+    return cur_row - 1;
+  }
   return cur_row;
 }
 
@@ -188,6 +225,11 @@ static unsigned int get_next_row(unsigned int cur_row, char c) {
 */
 static unsigned int get_next_col(unsigned int cur_col, char c) {
   // TODO: Implement this function.
+  if (in_chars(c, "<aA")) {
+    return cur_col - 1;
+  } else if (in_chars(c, ">dD")) {
+    return cur_col + 1;
+  }
   return cur_col;
 }
 
@@ -200,7 +242,12 @@ static unsigned int get_next_col(unsigned int cur_col, char c) {
 */
 static char next_square(game_state_t* state, unsigned int snum) {
   // TODO: Implement this function.
-  return '?';
+  snake_t* p = state->snakes + snum;
+  unsigned x = p->head_row;
+  unsigned y = p->head_col;
+  unsigned nx = get_next_row(x, get_board_at(state, x, y));
+  unsigned ny = get_next_col(y, get_board_at(state, x, y));
+  return get_board_at(state, nx, ny);
 }
 
 /*
@@ -216,6 +263,17 @@ static char next_square(game_state_t* state, unsigned int snum) {
 */
 static void update_head(game_state_t* state, unsigned int snum) {
   // TODO: Implement this function.
+  snake_t* p = state->snakes + snum;
+  unsigned x = p->head_row;
+  unsigned y = p->head_col;
+  char head_char = get_board_at(state, x, y);
+  unsigned nx = get_next_row(x, head_char);
+  unsigned ny = get_next_col(y, head_char);
+
+  p->head_row = nx;
+  p->head_col = ny;
+  set_board_at(state, nx, ny, head_char);
+  set_board_at(state, x, y, head_to_body(head_char));
   return;
 }
 
@@ -231,12 +289,37 @@ static void update_head(game_state_t* state, unsigned int snum) {
 */
 static void update_tail(game_state_t* state, unsigned int snum) {
   // TODO: Implement this function.
+  snake_t* p = state->snakes + snum;
+  unsigned x = p->tail_row;
+  unsigned y = p->tail_col;
+  char tail_char = get_board_at(state, x, y);
+  unsigned nx = get_next_row(x, tail_char);
+  unsigned ny = get_next_col(y, tail_char);
+
+  p->tail_row = nx;
+  p->tail_col = ny;
+  set_board_at(state, nx, ny, body_to_tail(get_board_at(state, nx, ny)));
+  set_board_at(state, x, y, ' ');
   return;
 }
 
 /* Task 4.5 */
 void update_state(game_state_t* state, int (*add_food)(game_state_t* state)) {
   // TODO: Implement this function.
+  for (unsigned i = 0; i < state->num_snakes; i++) {
+    char next_char = next_square(state, i);
+    if (is_snake(next_char) || next_char == '#') { // snake crash
+      state->snakes[i].live = false;
+      set_board_at(state, state->snakes[i].head_row, state->snakes[i].head_col, 'x');
+      continue;
+    }
+    update_head(state, i);
+    if (next_char != '*') {
+      update_tail(state, i);
+    } else { // eat a fruit
+      add_food(state);
+    }
+  }
 
   return;
 }
@@ -244,7 +327,28 @@ void update_state(game_state_t* state, int (*add_food)(game_state_t* state)) {
 /* Task 5 */
 game_state_t* load_board(char* filename) {
   // TODO: Implement this function.
-  return NULL;
+  FILE *f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "load_board: file not exist: %s\n", filename);
+    return NULL;
+  }
+  game_state_t* ret = (game_state_t*)malloc(sizeof(game_state_t));
+  char board_line[MAX_LINE_LEN], *p = NULL;
+  char* board_row[MAX_LINE_LEN] = {0};
+  unsigned row_idx = 0;
+
+  while ((p = fgets(board_line, sizeof(board_line), f)) != NULL) {
+    size_t len = strlen(board_line);
+    if (board_line[len - 1] == '\n') board_line[len - 1] = '\0';
+    char* bl = (char*)malloc((len + 1) * sizeof(char));
+    strcpy(bl, board_line);
+    board_row[row_idx++] = bl;
+  }
+  ret->num_rows = row_idx;
+  ret->board = (char**)malloc((row_idx) * sizeof(char*));
+  memcpy(ret->board, board_row, row_idx * sizeof(char*));
+
+  return ret;
 }
 
 /*
@@ -257,11 +361,48 @@ game_state_t* load_board(char* filename) {
 */
 static void find_head(game_state_t* state, unsigned int snum) {
   // TODO: Implement this function.
+
+  snake_t* snake = state->snakes + snum;
+  unsigned x = snake->tail_row;
+  unsigned y = snake->tail_col;
+  char cur_char = get_board_at(state, x, y);
+  while (!is_head(cur_char)) {
+    x = get_next_row(x, cur_char);
+    y = get_next_col(y, cur_char);
+    cur_char = get_board_at(state, x, y); 
+  }
+  snake->head_row = x;
+  snake->head_col = y;
+
   return;
 }
 
 /* Task 6.2 */
 game_state_t* initialize_snakes(game_state_t* state) {
   // TODO: Implement this function.
-  return NULL;
+  unsigned snakes[MAX_LINE_LEN][2] = {};
+  unsigned s_cnt = 0;
+  for (int i = 0; i < state->num_rows; i++) {
+    char *p = state->board[i];
+    char *ch = p;
+    while (*p) {
+      if (is_tail(*p)) {
+        snakes[s_cnt][0]= (unsigned)i;
+        snakes[s_cnt][1] = (unsigned)(p - ch);
+        s_cnt++;
+      }
+      p++;
+    }
+  }
+
+  state->num_snakes = s_cnt;
+  state->snakes = (snake_t*)malloc(s_cnt * sizeof(snake_t));
+  for (unsigned i = 0; i < s_cnt; i++) {
+    state->snakes[i].live = true;
+    state->snakes[i].tail_row = snakes[i][0];
+    state->snakes[i].tail_col = snakes[i][1];
+    find_head(state, i);
+  }
+
+  return state;
 }
